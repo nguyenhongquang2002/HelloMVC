4d4012d190329ecc467da7fa9527ee011673ab91
nguyenhongquang2002 <71063187+nguyenhongquang2002@users.noreply.github.com>
2022-02-25 19:30:03 +0700

    Add project files.

---
+++ HelloMVC/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using HelloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private MyDataContext db = new MyDataContext();
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+    }
+}
---
+++ HelloMVC/Models/MyDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class MyDataContext : DbContext
+    {
+        public MyDataContext():base("name=MyConnectionString")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
